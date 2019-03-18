4fde28148ab1d482eb7d4f256e0b27f77c63bf11
hantaozhang <hantaozhang@tencent.com>
2019-03-18 19:03:48 +0800

    优化slave 安装脚本

--- install_shell/slave/slave.sh
+++ install_shell/slave/slave.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
-
 set -o errexit
 
 # 当前目录
 ROOT_PATH=$(cd `dirname $0`; pwd)
-
 ETCD_URL="192.168.56.101:2379"
+MASTER_URL="192.168.56.101:8080"
 
 LOCAL_IP_ADDR=`ip a | grep inet | grep -v inet6 | grep -v 127 | grep 192.168.56 | sed 's/^[ \t]*//g' | cut -d ' ' -f2 | cut -d '/' -f1`
 
-MASTER_URL="192.168.56.101:8080"
-
-
+# 安装 kube-calio 网络扩展
 wget -O /usr/local/bin/calicoctl https://github.com/projectcalico/calicoctl/releases/download/v1.6.1/calicoctl
 chmod +x /usr/local/bin/calicoctl
-
-
 . kube-calico.sh ${ETCD_URL} ${LOCAL_IP_ADDR}
 
 # 下载k8s二进制包
@@ -28,11 +23,14 @@ wget ${DOWNLOAD_URL}
 # 解压k8s
 tar vxf kubernetes-server-linux-amd64.tar.gz
 
+# 复制kubectl
+cp "${ROOT_PATH}/kubernetes/server/bin/kubectl" /usr/local/bin/
+chmod +x /usr/local/bin/kubectl
+
 # kubelet安装
-cp kubernetes/server/bin/kubelet /usr/local/bin/kubelet
-chmox +x /usr/local/bin/kubelet
+cp "${ROOT_PATH}/kubernetes/server/bin/kubelet" /usr/local/bin/
+chmod +x /usr/local/bin/kubelet
 . kube-kubelet.sh ${MASTER_URL} ${LOCAL_IP_ADDR} ${ETCD_URL}
 
-
-
-
+# 清理下载的文件
+rm -rf ${ROOT_PATH}/kubernetes*

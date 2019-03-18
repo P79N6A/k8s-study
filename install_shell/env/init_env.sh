15f53a6d108859b3b32e6082f47e3ac233d6f8f2
hantaozhang <hantaozhang@tencent.com>
2019-03-18 17:45:50 +0800

    优化初始化环境脚本

--- install_shell/env/init_env.sh
+++ install_shell/env/init_env.sh
@@ -2,11 +2,24 @@
 
 set -o errexit
 
-# 关闭防火墙 和 
+# 关闭防火墙 和 SELINUX  disabled  enforcing$
 setenforce 0
-sed -i 's/^SELINUX=enforcing$/SELINUX= disabled/' /etc/selinux/config
-
+sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/g' /etc/selinux/config
 systemctl disable firewalld.service 
 systemctl stop firewalld.service
 
-reboot
+# 关闭swap
+swapoff -a
+sed '/dev\/mapper\/centos-swap/d'  /etc/fstab
+
+# k8s 配置文件
+cat > /etc/sysctl.d/k8s.conf <<EOF
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+vm.swappiness=0
+EOF
+sysctl -p /etc/sysctl.d/k8s.conf
+
+
+
